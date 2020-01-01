794dbcc7b5941b1e6be873e23b15efe53fb950be
Peter Roberts <peter@shortestpath.dev>
2020-01-01 17:06:42 +0000

    Convert stuff to module

--- 2019/day-2/part-2/lib/Advent/Intcode.pm
+++ 2019/day-2/part-2/lib/Advent/Intcode.pm
@@ -5,66 +5,97 @@ use warnings;
 use autodie ':all';
 use Const::Fast;
 use English '-no_match_vars';
+use Moo;
+use overload q{""} => \&as_str;
+use Carp;
 
 use Advent::Input;
 
 our $VERSION = v0.0.1;
 
-sub parse_input {
-    return parse_intcode_str($Advent::Input::INPUT);
-}
+has 'initial_state' => (
+    is       => 'ro',
+    required => 1,
+    init_arg => 'state',
+);
 
-sub parse_intcode_str {
-    my $s    = shift;
-    my @code = split ',', $s;
+has 'memory' => ( is => 'lazy', );
+
+has '_instr_ptr' => (
+    is      => 'rw',
+    default => 0,
+);
+
+sub _build_memory {
+    my $self = shift;
+    my $s    = $self->initial_state;
+    my @code = split q{,}, $s;
     chomp @code;
     return \@code;
 }
 
 const my $ADD => 1;
 const my $MUL => 2;
-const my $HLT => 3;
+const my $HLT => 99;
 
-const my $INSTR_LENGTH => 4;
+const my $BINARY_OP_LENGTH => 4;
 
-sub run_intcode {
-    my $code = shift;
-    my $pos  = 0;
+# Executes an Intcode until it completes.
+#
+# Throws exceptions on an unknown op code or overrunning memory without halting.
+# 
+# TODO add debugging.
+sub run {
+    my $self = shift;
 
-    while ( $pos < $code->@* ) {
-        my ( $op, @args ) = $code->@[ $pos .. $pos + 4 ];
+    while ( $self->_instr_ptr < $self->memory->@* ) {
+        my $op = $self->memory->[ $self->_instr_ptr ];
 
-        last                    if $op == $HLT;
-        run_add( $code, @args ) if $op == $ADD;
-        run_mul( $code, @args ) if $op == $MUL;
+        # TODO move to dispatch table off the op code
+        return if $op == $HLT;
+        $self->_add(), next if $op == $ADD;
+        $self->_mul(), next if $op == $MUL;
 
-        $pos += $INSTR_LENGTH;
+        croak "Unknown op code $op";
     }
 
+    croak "instruction pointer has overrun the end of memory";
+}
+
+# TODO generalise the implementation of binary operators
+
+# Executes an on the current Intcode state.
+sub _add {
+    my $self = shift;
+
+    my ( $addr_1, $addr_2, $result_addr ) = $self->memory->@[ $self->_instr_ptr + 1 .. $self->_instr_ptr + 4 ];
+    my ( $val_1, $val_2 ) = $self->memory->@[ $addr_1, $addr_2 ];
+    $self->memory->[$result_addr] = $val_1 + $val_2;
+
+    # increment the intstruction pointer
+    $self->_instr_ptr( $self->_instr_ptr + $BINARY_OP_LENGTH );
+
     return;
 }
 
-sub run_add {
-    my ( $code, $op_1, $op_2, $store ) = @_;
-    $code->[$store] = $code->[$op_1] + $code->[$op_2];
+# Executes a multiply on the current Intcode state.
+sub _mul {
+    my $self = shift;
+
+    my ( $addr_1, $addr_2, $result_addr ) = $self->memory->@[ $self->_instr_ptr + 1 .. $self->_instr_ptr + 4 ];
+    my ( $val_1, $val_2 ) = $self->memory->@[ $addr_1, $addr_2 ];
+    $self->memory->[$result_addr] = $val_1 * $val_2;
+
+    # increment the intstruction pointer
+    $self->_instr_ptr( $self->_instr_ptr + $BINARY_OP_LENGTH );
+
     return;
 }
 
-sub run_mul {
-    my ( $code, $op_1, $op_2, $store ) = @_;
-    $code->[$store] = $code->[$op_1] * $code->[$op_2];
-    return;
-}
-
-sub run_main {
-    my $code = parse_intcode_file('input.txt');
-
-    # apply previous state
-    $code->[1] = 12;
-    $code->[2] = 2;
-
-    run_intcode($code);
-    print "$code->[0]\n" or die $!;
+# Returns a string representation of the current Intcode state.
+sub as_str {
+    my $self = shift;
+    return join q{,}, $self->memory->@*;
 }
 
 1;
